403c9409fb126c64744ca5074fa423347a4c7317
Matias Laakso <62062625+Voidrius@users.noreply.github.com>
2023-09-07 17:52:12 +0300

    Fixed logic issues

--- Kide.Bot.ps1
+++ Kide.Bot.ps1
@@ -2,6 +2,11 @@
 Get-Location | Set-Location
 $ProgressPreference = "Continue"
 $runFetch = $False
+$specificTicket = $null
+$specificTicketOrdered = $False
+$eventKey = $null
+$prioLooped = $False
+$script:timesOrdered = 0
 
 # Fetches auth.key from key.txt
 $userKey =  Get-Content "key.txt"
@@ -13,29 +18,21 @@ If ($userKey -eq "<your bearer token here>") {
 }
 
 # Acquire event link from user
-$eventKey = Read-Host "Enter the event link"
+While (!$eventkey){
+  $eventKey = Read-Host "Enter the event link"
+  If ($eventKey -eq ""){
+    Write-Host "Please enter a valid link"`n
+  }
+} 
 
-Write-Host `n"This script prioritizes a specific ticket name that you can enter yourself."
+Write-Host `n"It's highly recommended to be signed in through Haka in Kide.App, in case any tickets require it.`nThe bot is not able to order tickets that have been marked as student tickets otherwise."`n
+Write-Host "This script prioritizes a specific ticket name that you can enter yourself."`n
 Write-Host "For example, you can try the start time of an appro, `nor a bars name where you want to end up for the after party"`n
 Write-Host "It tries to get the maximum allowed amount of the ticket you specified,`nafterwhich it tries to get any other ticket type as well."`n
 Write-Host "If you don't know any names that could be related to a ticket you want, `nyou can also just leave it empty by pressing enter during the prompt."`n
-Write-Host "Organization member tickets are now supported too, but they hold a high risk,`nsince they are the most prone to crashing the script."`n
-Write-Host "You can choose to ignore organization tickets completely, or if you are a member of an organization,`nyou can try to filter the search with your orgs name in the ticket name prompt."`n 
+Write-Host "You can choose to not prioritize organization tickets, or if you are a member of an organization,`nyou can try to filter the search with your orgs name in the ticket name prompt."`n 
 Write-Host "Note: This only matters if the event organizer has set the ticket type as a member ticket, `nthe script will not recognize memberships that are not required by Kide.App itself."`n 
-Write-Host "However, the safest bet is to ignore organization tickets, or running two instances of this script at the same time,`nwith only the other one scanning for organization tickets." `n`n
 Write-Host "Side note: Only use small letters in the prompts to be safe, `nthis is a delicate machine, and the kide.api is even more fragile."`n`n`n 
-# Prompt for ticket name
-$keyWord = Read-Host "Enter a keyword that might be included in the ticket name (Name of the bar for example)"
-
-# Prompt for jasenyys ticket
-$jasenCheck = $null
-Do {
-  $jasenPrompt = Read-Host "Do you want an organization ticket? `nThis is more unstable, but should atleast get you an organization ticket if nothing else. (y/n)"
-  If (($jasenPrompt -ne "y") -And ($jasenPrompt -ne "n")) {
-    Write-Host "Invalid input."
-  }
-  Else {$jasenCheck = $jasenPrompt}
-} until ($jasenCheck)
 
 # Format the passed event key into a suitable form for http requests
 $eventKey = $eventKey.Split('/')[4]
@@ -44,6 +41,19 @@ $requestLink = "https://api.kide.app/api/products/" + $eventKey
 # Finds the events data with a http request, and formats into a ps.object for easy access of data
 $getData = Invoke-WebRequest -Uri $requestLink -UseBasicParsing -Method "GET"| ConvertFrom-Json | ConvertTo-Json -depth 100
 $ticketData = $getData | ConvertFrom-Json
+
+# Prompt for ticket name
+$keyWord = Read-Host "Enter a keyword that might be included in the ticket name (Name of the bar for example)"
+
+# Prompt for jasenyys ticket
+$jasenCheck = $null
+Do {
+  $jasenPrompt = Read-Host `n"Do you want to prioritize an organization ticket? (y/n)"
+  If (($jasenPrompt -ne "y") -And ($jasenPrompt -ne "n")) {
+    Write-Host "Invalid input."
+  }
+  Else {$jasenCheck = $jasenPrompt}
+} until ($jasenCheck)
 
 # Formats POST request parameters in advance
 $target = "https://api.kide.app/api/reservations"
@@ -73,6 +83,7 @@ Do {
   }
   # Starts looping this until the ticket data is available.
   # Fetches ticket data and stores it into a hashtable, also checks for when the tickets become available
+  Start-Sleep -Milliseconds 500
   $ProgressPreference = "silentlyContinue"
   $getData2 = Invoke-WebRequest -Uri $requestLink -UseBasicParsing -Method "GET" | ConvertFrom-Json | ConvertTo-Json -depth 100
   $ticketData = $getData2 | ConvertFrom-Json
@@ -80,14 +91,13 @@ Do {
   
 } until ($ticketState -ne 0)
 
-# Check for the type of script the user wanted
-
-# Specific ticket script
+# Specific ticket script, resets variables
 $keyWord = "*"+$keyWord+"*"
 $specificDone = $False
 $ticketFound = $False
+$orgFound = $False
 $ticketID = 0
-# Core of the script
+# Core of the specific ticket script
 :specificScript while ($specificDone -eq $False) {
   # Try to find the specific ticket
   :searcherScript while ($ticketFound -eq $False) {
@@ -98,7 +108,7 @@ $ticketID = 0
           $orgFound = $True
           Break searcherScript
         }
-        # Scrapes through the rest to look for a member ticket in the same location
+        # Scrapes through the rest to look for a organization ticket with the same keyword
         Else {
           $scrapeID = $ticketID
           while ($scrapeID -ne $ticketState){
@@ -198,9 +208,12 @@ $ticketID = 0
         
   } | ConvertTo-Json -Depth 2
 
-  # Runs the requests as a background job and starts to form a new one until ticket types run out. 
+  # Runs the requests as a background job
   try {
   Start-Job -ScriptBlock {Invoke-WebRequest -UseBasicParsing -Uri $using:target -Headers $using:header -Method "POST" -Body $using:payload -ContentType "application/json" }
+  $specificTicketOrdered = $True
+  $specificTicket = $ticketID
+  $timesOrdered++
   }
   catch {
       $errorMessage = $_.Exception.Message
@@ -220,75 +233,137 @@ $ticketID = 0
 $specificDone = $True
 $runFetch = $True
 }
-
-# Runs if permission is granted
-If ($runFetch -eq $True){
+function Fetch_Script {
   $ticketID = 0
   # Core of the script
   # "Fetch anything you can" version of the script
-  while ($ticketID -ne $ticketState) {
-      # This part loops through all the tickets and gets their ID and maximum reservable count
-      If ($jasenCheck -eq "n"){
-        If ($ticketData.model.variants[$ticketID].isProductVariantMembershipRequired){
-          $ticketID++
-          continue
-        }  
-      }
-      
-      # Logic for determining ticket amount
-      If ($checkoutMax){
-        $max = $checkoutMax
-      }
-      Else {$max = $ticketData.model.variants[$ticketID].productVariantMaximumReservableQuantity}
-      $available = $ticketData.model.variants[$ticketID].availability
-      $inventoryId = $ticketData.model.variants[$ticketID].inventoryId
-      If ($max -gt $available) {
-        If ($available -eq 0 ) {
-          $ticketID++; continue
-        }
-        Else {$max = $available}
-      }
+  :fetchScript while ($ticketID -ne $ticketState) { 
     
-        # Creates the json payload for the POST request with our data    
-        $payload = [ordered]@{   
-          expectCart = $true
-          includeDeliveryMethods = $false
-          toCancel = @()
-          toCreate =  @(
-            [ordered]@{
-                  inventoryId = $inventoryId
-                  quantity = $max
-                  productVariantUserForm = $null
-                }
-              )
-
-        } | ConvertTo-Json -Depth 2
-
-      # Runs the requests as a background job and starts to form a new one until ticket types run out. 
-      
-      try {
-      Start-Job -ScriptBlock {Invoke-WebRequest -UseBasicParsing -Uri $using:target -Headers $using:header -Method "POST" -Body $using:payload -ContentType "application/json" }
+    # Checks if the keyword script already ordered this ticket
+    If ($specificTicketOrdered -eq $True){
+      If ($ticketID -eq $specificTicket){
+        $ticketID++
+          continue
       }
-      catch {
-          $errorMessage = $_.Exception.Message
-          if (Get-Member -InputObject $_.Exception -Name 'Response') {
-              try {
-                  $result = $_.Exception.Response.GetResponseStream()
-                  $reader = New-Object System.IO.StreamReader($result)
-                  $reader.BaseStream.Position = 0
-                  $reader.DiscardBufferedData()
-                  $responseBody = $reader.ReadToEnd();
-              } catch {
-                  Throw "An error occurred while calling POST method at: $target. Error: $errorMessage. Cannot get more information."
-              }
+    }
+    
+    # Checks ticket priority and allows it through if appropriate
+    $jasenLoop = $False
+    
+    :jasenCheck while ($jasenLoop -eq $False) {
+      If ($ticketData.model.variants[$ticketID].isProductVariantMembershipRequired) {
+        If ($jasenCheck -eq "n") {
+          If ($prioLooped -eq $False) {
+            $ticketID++
+            break jasenCheck
           }
-          Throw "An error occurred while calling POST method at: $target. Error: $errorMessage. Response body: $responseBody"
+          Else {
+            $jasenLoop = $True
+            break jasenCheck
+          }
+        }
+        Elseif ($ticketData.model.variants[$ticketID].isProductVariantMembershipRequired) {
+          If ($prioLooped -eq $False){
+            break jasenCheck
+          }
+          Else {
+            $jasenLoop = $True
+            $ticketID++
+            continue
+          }  
+        }
+        Else {
+          $jasenLoop = $True
+          continue
+        }
       }
-    $ticketID++
+      Else {$jasenLoop = $True}
+    }
+    
+    # Catches stray runs
+    If ($ticketID -eq $specificTicket) {
+      $ticketID++; continue
+    }
 
-   }
+    # Logic for determining ticket amount
+    If ($checkoutMax){
+      $max = $checkoutMax
+    }
+    Else {
+      $max = $ticketData.model.variants[$ticketID].productVariantMaximumReservableQuantity
+    }
+
+    $available = $ticketData.model.variants[$ticketID].availability
+    $inventoryId = $ticketData.model.variants[$ticketID].inventoryId
+
+    If ($max -gt $available) {
+      If ($available -eq 0 ) {
+        $ticketID++; continue
+      }
+      Else {$max = $available}
+    }
+  
+    # Creates the json payload for the POST request with our data    
+    $payload = [ordered]@{   
+      expectCart = $true
+      includeDeliveryMethods = $false
+      toCancel = @()
+      toCreate =  @(
+        [ordered]@{
+              inventoryId = $inventoryId
+              quantity = $max
+              productVariantUserForm = $null
+            }
+          )
+
+    } | ConvertTo-Json -Depth 2
+
+    # Runs the requests as a background job and starts to form a new one until ticket types run out. 
+    
+    try {
+      $script:timesOrdered++
+      Start-Sleep -Milliseconds 700
+      Start-Job -ScriptBlock {Invoke-WebRequest -UseBasicParsing -Uri $using:target -Headers $using:header -Method "POST" -Body $using:payload -ContentType "application/json" }
+    }
+    catch {
+        $errorMessage = $_.Exception.Message
+        if (Get-Member -InputObject $_.Exception -Name 'Response') {
+            try {
+                $result = $_.Exception.Response.GetResponseStream()
+                $reader = New-Object System.IO.StreamReader($result)
+                $reader.BaseStream.Position = 0
+                $reader.DiscardBufferedData()
+                $responseBody = $reader.ReadToEnd();
+            } catch {
+                Throw "An error occurred while calling POST method at: $target. Error: $errorMessage. Cannot get more information."
+              } 
+        }
+        Throw "An error occurred while calling POST method at: $target. Error: $errorMessage. Response body: $responseBody"
+    }
+    $ticketID++ 
+  }
+  $script:funcRuns += 1
+  $prioLooped = $True
 }
+
+$funcRuns = 0
+# Runs if permission is granted
+# Loops through according to users org ticket prompt, and a second time disregarding it
+If ($runFetch -eq $True) {
+  while ($funcRuns -lt 2) {
+    Fetch_Script
+    $prioLooped = $True
+  }
+}
+
 
 # Creates a info pop up when the script is done.
 $popup = New-Object -ComObject Wscript.Shell
-$popup.Popup("Refresh the kide.app page, the tickets have been added to your shopping cart. Be sure to buy them before the reservation time ends!",0,"Done",0x1)
+If($timesOrdered -eq 0){
+  $popup.Popup("The bot failed to order any tickets. The cause is usually that all of the tickets were sold out. I'm sorry for the inconvenience :(",0,"Sorry :(",0x1)
+}
+Else{
+  $popup.Popup("Refresh the kide.app page, the tickets have been added to your shopping cart. Be sure to buy them before the reservation time ends!",0,"Done",0x1)
+}
+
+
